3f16eb6ecd710bc9d74201328cafe5c4ad252b4c
Curtis Larson <quackware@gmail.com>
2018-11-15 08:48:09 -0800

    Refresh copy and skills

--- js/contact-modal.js
+++ js/contact-modal.js
@@ -5,9 +5,10 @@ $("#contactModal").on("show.bs.modal", function (event) {
   var modal = $(this);
   var subject = "";
   var message = "Here's some information about what I'm looking for:\n\n";
-  message += "Project Description:\n";
-  message += "Budget: \n";
-  message += "Additional Notes:\n";
+  message += "Project Description:\n\n\n";
+  message += "Availability:\n\n\n";
+  message += "Budget: \n\n\n";
+  message += "Additional Notes:";
   switch (type) {
     case "web-development":
       subject = "Custom Software & Web Development";
@@ -19,9 +20,9 @@ $("#contactModal").on("show.bs.modal", function (event) {
       message = "I'm interested in your JavaScript Consulting Services! " +
         message;
       break;
-    case "android":
-      subject = "Android Services";
-      message = "I'm interested in your Android Services! " + message;
+    case "enterprise":
+      subject = "Enterprise Services";
+      message = "I'm interested in your Enterprise Services! " + message;
       break;
     case "consulting":
       subject = "General Consulting Services";
